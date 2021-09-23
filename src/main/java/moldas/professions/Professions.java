e4096fc04e2e39da3b8ba4f0f04b68be29909deb
0AT13 <anton13griogruk@gnail.com>
2021-09-23 17:02:05 +0300

    Code cleaning

--- src/main/java/moldas/professions/Professions.java
+++ src/main/java/moldas/professions/Professions.java
@@ -6,7 +6,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.UUID;
