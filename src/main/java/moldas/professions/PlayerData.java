1ab76173caeebde24dceac857046844aa3ed3313
0AT13 <anton13griogruk@gnail.com>
2021-09-24 10:27:30 +0300

    [Changed] Deleted hashmap for characteristics of player [Addded] TODO for new idea

--- src/main/java/moldas/professions/PlayerData.java
+++ src/main/java/moldas/professions/PlayerData.java
@@ -1,11 +1,15 @@
 package moldas.professions;
 
+import java.sql.Struct;
 import java.util.HashMap;
 
 public class PlayerData {
 
     public String playerName;
     public HashMap <String, String> playerProfession = new HashMap<>();
+
+    //TODO Create parameters for characteristics
+
     public int primaryProfLvl = 0;
     public int secondaryProfLvl = 0;
     public float speed = (float) 0.2;
@@ -19,5 +23,12 @@ public class PlayerData {
 
     PlayerData(String _playerName) {
         playerName = _playerName;
+
+        //playerCharacteristics.put("Athletic", 1);
+        //playerCharacteristics.put("Acrobatic", 1);
+        //playerCharacteristics.put("Strength", 1);
+        //playerCharacteristics.put("Blocking", 1);
+        //playerCharacteristics.put("Vitality", 1);
+        //playerCharacteristics.put("Stealth", 1);
     }
 }
--- src/main/java/moldas/professions/Professions.java
+++ src/main/java/moldas/professions/Professions.java
@@ -3,7 +3,6 @@ package moldas.professions;
 import moldas.professions.commands.*;
 import moldas.professions.prof.listners.GlobalListners;
 import moldas.professions.prof.listners.Miner;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Professions extends JavaPlugin {
