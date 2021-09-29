86f6d9dfa66716ec7695c73896fa9bb1f06673b2
0AT13 <anton13griogruk@gnail.com>
2021-09-29 10:58:53 +0300

    Refactoring

--- src/main/java/moldas/professions/prof/data/BlacksmithData.java
+++ src/main/java/moldas/professions/prof/data/BlacksmithData.java
@@ -3,4 +3,6 @@ package moldas.professions.prof.data;
 public class BlacksmithData {
     public final static String PROF_TYPE = "Secondary";
     public final static String PROF_NAME = "Blacksmith";
+
+    public final static String[] BUTTON_LORE = {"Blacksmith button lore"};
 }
--- src/main/java/moldas/professions/prof/data/FarmerData.java
+++ src/main/java/moldas/professions/prof/data/FarmerData.java
@@ -3,4 +3,6 @@ package moldas.professions.prof.data;
 public class FarmerData {
     public final static String PROF_TYPE = "Primary";
     public final static String PROF_NAME = "Farmer";
+
+    public final static String[] BUTTON_LORE = {"Farmer button lore", "Next line of lore"};
 }
