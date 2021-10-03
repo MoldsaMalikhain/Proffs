99cb85e5a40d80f672daa2e545465a4706b849f6
ximure <ximure@mail.ru>
2021-10-03 03:23:00 +0300

    Exceptions shenanigans

---
+++ src/main/java/moldas/professions/exceptions/SetPlayerException.java
@@ -0,0 +1,10 @@
+package moldas.professions.exceptions;
+
+public class SetPlayerException extends Exception {
+    public SetPlayerException() {
+    }
+
+    public SetPlayerException(String message) {
+        super(message);
+    }
+}
